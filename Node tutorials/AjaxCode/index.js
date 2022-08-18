f3be6ba0bfeb5e98aa5a4db80959849dc3359f4d
varunbgit <vbansalevents01@gmail.com>
2022-08-18 23:02:08 +0530

    Adding the index.js

--- Node tutorials/AjaxCode/index.js
+++ Node tutorials/AjaxCode/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const  myXML = XMLHttpRequest();
+        myXML.open("GET","__dirname+'/hello.html'","false"); file:///Users/varunbansal/Documents/Mern%20Stack%20training/Node%20tutorials/AjaxCode/hello.html
+        myXML.send();
+        
+
+
+
+
+
+
+
+
 app.get("/" , function(req,res){
    res.sendFile("index.html"); 
 });
