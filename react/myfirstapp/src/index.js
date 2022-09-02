c2507cda3629575375bc0cdcd0ffdfa461293261
varunbgit <vbansalevents01@gmail.com>
2022-09-02 23:12:24 +0530

    Using multiple tags in render and use of {} in react

--- react/myfirstapp/src/index.js
+++ react/myfirstapp/src/index.js
@@ -2,9 +2,15 @@ var React = require("react")
 var ReactDOM =require("react-dom")
 
 
+var fname = "Varun"
+var lname = "Bansal"
+
 
 ReactDOM.render(
-    <h1>Hello my name is Varun Bansal</h1>
+    <div>
+        <h1>Hello I m Developer</h1>
+        <p>My name is {fname}{lname}</p>
+    </div>
     ,
     document.getElementById("root")
 )
