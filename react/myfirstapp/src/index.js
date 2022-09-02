3943039c6b7e1831659b56234416e86bbd77810e
varunbgit <vbansalevents01@gmail.com>
2022-09-02 23:15:57 +0530

    Adding the date or current year line using JS

--- react/myfirstapp/src/index.js
+++ react/myfirstapp/src/index.js
@@ -4,12 +4,14 @@ var ReactDOM =require("react-dom")
 
 var fname = "Varun"
 var lname = "Bansal"
-
+const date = new Date()
+var year = date.getFullYear() ;
 
 ReactDOM.render(
     <div>
         <h1>Hello I m Developer</h1>
         <p>My name is {fname}{lname}</p>
+        <p>Current year is { year }</p>
     </div>
     ,
     document.getElementById("root")
